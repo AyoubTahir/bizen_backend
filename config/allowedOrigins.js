4dacdba299f8853cc92f1be862b4b1043a668f42
AyoubTahir <ayoub.tahir.2021@gmail.com>
2023-01-09 16:33:05 +0100

    adding allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://localhost:3000",
   "http://127.0.0.1:5173",
+  "http://bizen-app-ayoubtahir.vercel.app",
+  "https://bizen-app-ayoubtahir.vercel.app",
 ];
 
 export default allowedOrigins;
